5cea58303361aba4e94ae021fec51aa86a4279af
Karli Vare <karlivare@Karlis-MacBook-Pro.local>
2023-09-25 18:45:45 -0400

    Added function on bird collision.

--- week4/sprite.js
+++ week4/sprite.js
@@ -7,6 +7,21 @@ class Sprite{
         this.len = this.animation.length;
         this.speed = speed; 
         this.index = 0;
+        this.radius = this.w/2;
+
+        //defining function within a constructor makes it specific to that instance
+        this.intersects = function(other){
+                //the x in this x represents the center
+                let d = dist(this.x + this.radius, this.y + this.radius, other.x + other.radius, other.y + other.radius);
+                if (d<this.radius + other.radius){
+                    return true;
+                } else{
+                    return false;
+                } 
+                
+                
+        }
+        
     }
 
     show(){
@@ -20,7 +35,8 @@ class Sprite{
 
         //once bird is off the screen, reset the x position
         if(this.x >width){
-            this.x = -this.w;""
+            this.x = -this.w;
         }
     }
+
 }
